4e0e943894ef394decd8a0f21c864162aba4b576
dmitry <spacedmi@gmail.ru>
2016-01-20 00:46:40 +0300

    Computing time parameter from command line argument

--- heart-visualization-multi/main.cpp
+++ heart-visualization-multi/main.cpp
@@ -1,17 +1,34 @@
 #include "Solver.h"
 #include "MyHeart.h"
+#include <stdlib.h>
 
 #define D_dt 0.01
-#define D_maxT 1000.0
 #define D_count_dt_till_save 100
 
 int main(int argc, char *argv[])
 {
+    MPI_Init(&argc, &argv);
+
+    double D_maxT = 100.0;
+
+    if (argc > 1)
+    {
+        D_maxT = atof(argv[1]);
+    }
+    else
+    {
+        int ProcRank;
+        MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+
+        if (ProcRank == 0)
+        {
+            printf("WARNING: Program needs 1 argument: computing time! Starting with default D_maxT...\n");
+        }
+    }
+
     MyHeart* ode = new MyHeart();
 
     Solver solver(ode, D_dt, D_maxT, D_count_dt_till_save);
-
-    MPI_Init(&argc, &argv);
 
     solver.MultiIntegrate();
 
